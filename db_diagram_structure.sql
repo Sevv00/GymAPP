ac44cb8b8cc56d4774160ae8e581eb1293ec60a5
Juliusz Brodnicki <juliusbrodnicki@gmail.com>
2025-06-03 00:18:48 +0200

    Database structure update, changed some of data types

--- db_diagram_structure.sql
+++ db_diagram_structure.sql
@@ -5,19 +5,17 @@ Project GymManagement {
 Table Users {
   id SERIAL [pk]
   email VARCHAR(255) [unique]
-  phone_country_code VARCHAR(3)
-  mobile_phone_number VARCHAR(9)
-  password_hash VARCHAR(255)
-  first_name VARCHAR(100)
-  last_name VARCHAR(100)
-  role ENUM('ADMIN', 'EMPLOYEE', 'CLIENT')
-  created_at TIMESTAMP
+  phone_number VARCHAR(15) [unique]
+  password_hash VARCHAR(255) [not null]
+  first_name VARCHAR(25)
+  last_name VARCHAR(50)
+  role ENUM('ADMIN', 'EMPLOYEE', 'CLIENT') [not null]
+  created_at TIMESTAMP [default: 'CURRENT_TIMESTAMP']
   last_logged_in_date TIMESTAMP
   is_google BOOLEAN
-  discount ENUM('NONE', 'STUDENT', 'MULTISPORT')
-  google_id VARCHAR(255) [unique]
-  ad_agreement BOOLEAN
-  is_active BOOLEAN
+  discount ENUM('NONE', 'STUDENT', 'MULTISPORT') [default: 'NONE']
+  ad_agreement BOOLEAN 
+  is_active BOOLEAN [default: true]
   avatar BYTEA
 }
 
@@ -26,38 +24,38 @@ Table Offers {
   name VARCHAR(100)
   description TEXT
   price_text VARCHAR(100)
-  price DECIMAL(10, 2)
-  duration_days INT
-  is_permanent BOOLEAN
+  price MONEY [not null]
+  duration_days INT [not null]
+  is_permanent BOOLEAN [not null]
   offer_expired_date TIMESTAMP
-  created_at TIMESTAMP
+  created_at TIMESTAMP [default: 'CURRENT_TIMESTAMP']
   created_by INT [ref: > Users.id]
-  is_active BOOLEAN
+  is_active BOOLEAN [default: true]
 }
 
 Table Classes {
-  id SERIAL [pk]
-  offer_id INT [ref: > Offers.id]
+  id SERIAL [pk] 
+  offer_id INT [ref: > Offers.id, not null]
   description TEXT
   start_time TIMESTAMP
   end_time TIMESTAMP
   instructor_id INT [ref: > Users.id]
-  capacity INT
+  capacity SMALLINT
 }
 
 Table ClassRegistrations {
   id SERIAL [pk]
-  user_id INT [ref: > Users.id]
-  class_id INT [ref: > Classes.id]
-  registered_at TIMESTAMP
-  is_active BOOLEAN
+  user_id INT [ref: > Users.id, not null]
+  class_id INT [ref: > Classes.id, not null]
+  registered_at TIMESTAMP [default: 'CURRENT_TIMESTAMP']
+  is_active BOOLEAN [default: true]
 }
 
 Table Purchases {
   id SERIAL [pk]
-  user_id INT [ref: > Users.id]
-  offer_id INT [ref: > Offers.id]
-  purchase_date TIMESTAMP
+  user_id INT [ref: > Users.id, not null]
+  offer_id INT [ref: > Offers.id, not null]
+  purchase_date TIMESTAMP [default: 'CURRENT_TIMESTAMP']
   valid_until TIMESTAMP
 }
 
@@ -66,27 +64,24 @@ Table GuestActions {
   first_name VARCHAR(100)
   last_name VARCHAR(100)
   email VARCHAR(255)
-  phone_country_code VARCHAR(3)
-  mobile_phone_number VARCHAR(9)
-  purchase_id INT [ref: > Purchases.id]
-  created_at TIMESTAMP
+  phone_number VARCHAR(15)
+  purchase_id INT [ref: > Purchases.id, not null]
+  created_at TIMESTAMP [default: 'CURRENT_TIMESTAMP']
 }
 
 Table TrainingPlans {
   id SERIAL [pk]
-  user_id INT [ref: > Users.id]
+  user_id INT [ref: > Users.id, not null]
   created_by INT [ref: > Users.id]
-  created_at TIMESTAMP
-  is_active BOOLEAN
+  created_at TIMESTAMP [default: 'CURRENT_TIMESTAMP']
 }
 
 Table Workouts {
   id SERIAL [pk]
-  user_id INT [ref: > Users.id]
+  user_id INT [ref: > Users.id, not null]
   trainigplan_id INT [ref: > TrainingPlans.id]
   created_by INT [ref: > Users.id]
-  created_at TIMESTAMP
-  is_active BOOLEAN
+  created_at TIMESTAMP [default: 'CURRENT_TIMESTAMP']
 }
 
 Table ExerciseIcons {
@@ -100,32 +95,31 @@ Table Exercises {
   name VARCHAR(100)
   user_id INT [ref: > Users.id]
   created_by INT [ref: > Users.id]
-  created_at TIMESTAMP
-  is_active BOOLEAN
+  created_at TIMESTAMP [default: 'CURRENT_TIMESTAMP']
   icon_id INT [ref: > ExerciseIcons.id]
 }
 
 Table Sets {
   id SERIAL [pk]
-  exercise_id INT [ref: > Exercises.id]
-  workout_id INT [ref: > Workouts.id]
-  number_of_sets INT
+  exercise_id INT [ref: > Exercises.id, not null]
+  workout_id INT [ref: > Workouts.id, not null]
+  number_of_sets SMALLINT
 }
 
 Table ExerciseSets {
   id SERIAL [pk]
-  set_id INT [ref: > Sets.id]
-  workout_id INT [ref: > Exercises.id]
-  number_of_reps INT
-  weight INT
-  time TIMESTAMP
+  set_id INT [ref: > Sets.id, not null]
+  workout_id INT [ref: > Exercises.id, not null]
+  number_of_reps SMALLINT
+  weight SMALLINT
+  duration INTERVAL 
 }
 
 Table TrainingLogs {
   id SERIAL [pk]
-  user_id INT [ref: > Users.id]
-  workout_id INT [ref: > Workouts.id]
-  date DATE
+  user_id INT [ref: > Users.id, not null]
+  workout_id INT [ref: > Workouts.id, not null]
+  training_date TIMESTAMP [default: 'CURRENT_TIMESTAMP']
   content JSON
 }
 
@@ -134,21 +128,19 @@ Table Announcements {
   employee_id INT [ref: > Users.id]
   title VARCHAR(255)
   content TEXT
-  created_at TIMESTAMP
+  created_at TIMESTAMP [default: 'CURRENT_TIMESTAMP']
   created_by INT [ref: > Users.id]
-  is_active BOOLEAN
+  is_active BOOLEAN [default: true]
 }
 
 Table GymInfo {
   id SERIAL [pk]
-  opening_hours TEXT
-  description TEXT
-  phone_country_code_1 VARCHAR(3)
-  phone_number_1 CHAR(9)
-  phone_country_code_2 VARCHAR(3)
-  phone_number_2 CHAR(9)
-  email_1 VARCHAR(255)
+  opening_hours TEXT [not null]
+  description TEXT [not null]
+  phone_number_1 CHAR(15) [not null]
+  phone_number_2 CHAR(15)
+  email_1 VARCHAR(255) [not null]
   email_2 VARCHAR(255)
-  updated_by INT [ref: > Users.id]
-  updated_at TIMESTAMP
+  updated_by INT [ref: > Users.id, not null]
+  updated_at TIMESTAMP [default: 'CURRENT_TIMESTAMP']
 }
